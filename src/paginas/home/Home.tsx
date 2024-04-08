804762d1f6522cc2d8ad77bd7d8f2ab5160425a6
RodolfoOcanha <rodolfo.ocanha@hotmail.com>
2024-04-08 17:00:25 -0300

    React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <p className='text'>
+            
+            Meu primeiro projeto react!
+ 
+
+
+            
+            </p>
+            <img src="https://i.imgur.com/cufIziI.gif" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
